04bf07e9864943088020ccc54c86c40c56787d2a
senaysenturk <senaysenturk@outlook.com>
2018-06-25 15:33:10 +0300

    last commit

--- resources/views/layouts/partials/navigation.blade.php
+++ resources/views/layouts/partials/navigation.blade.php
@@ -1,4 +1,8 @@
 <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow" style="margin-bottom: 0">
     <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="{{ route('reservations.index') }}">Rezervasyon Sistemi</a>
     <input class="form-control form-control-dark w-100" type="text" placeholder="Ara" aria-label="Search">
+    <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+        </li>
+    </ul>
 </nav>
